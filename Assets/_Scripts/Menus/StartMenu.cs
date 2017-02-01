60eadc2f9eba87188d560c7308c17defb7ac798a
PaulLand <castingflame@gmail.com>
2017-02-01 07:45:54 +0000

    Player Prefs exists - In progress

---
+++ Assets/_Scripts/Menus/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Ask PPM to setup Persistant Storage
+        PlayerPrefsManager.Initialise();
+
+
+    }
+	
+
+}
